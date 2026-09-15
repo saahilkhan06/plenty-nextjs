2053b3ffa907aff8142808b40e155198bf6034d3
saahil khan <saahilslaptop@gmail.com>
2026-09-15 21:23:38 +0530

    responsive

--- app/Front/Components/Headnav/Headnav.tsx
+++ app/Front/Components/Headnav/Headnav.tsx
@@ -37,110 +37,128 @@ export default function Headnav() {
     <div className=" text-black bg-white">
       {/* <Header /> */}
       <div className="border-b bg-white">
-        <div className="mx-auto flex max-w-\[1400px\] flex-wrap items-center justify-between gap-4 px-5 py-4">
+        {/* Header */}
+        <div className="mx-auto flex max-w-[1400px] items-center justify-between gap-2 px-3 py-2 sm:gap-4 sm:px-5 sm:py-3 lg:gap-6 lg:py-4">
           {/* Logo */}
-          <div className="flex items-center gap-3">
+          <div className="flex shrink-0 items-center">
             <Link href="/Front">
               <Image
                 src="/assets/img/plenty-logo-2.png"
                 alt="Plenty Holidays Logo"
                 width={300}
                 height={50}
-                className="ml-10"
+                priority
+                className="
+            h-auto
+            w-[145px]
+            sm:w-[190px]
+            lg:w-[300px]
+            lg:ml-10
+          "
               />
             </Link>
           </div>
 
           {/* Right cluster */}
-          <div className="flex flex-1 items-center justify-between gap-6 lg:flex-none lg:gap-10">
-            <div className="flex items-center  gap-2">
-              <img
+          <div className="flex min-w-0 items-center gap-2 sm:gap-4 lg:gap-8">
+            {/* ATOL */}
+            <div className="flex shrink-0 items-center gap-2">
+              <Image
                 src="/assets/img/ATOL.png"
                 alt="ATOL Protected"
-                className="h-12"
                 width={56}
                 height={56}
+                className="h-8 w-auto sm:h-10 lg:h-12"
               />
+
               <span className="hidden lg:flex flex-col text-[13px] leading-tight">
-                <strong className="text-xl">ATOL</strong>{""}
-                protected
+                <strong className="text-xl">ATOL</strong>
+                <span>protected</span>
               </span>
             </div>
 
-            <div className="flex items-center gap-2">
+            {/* TTA */}
+            <div className="flex shrink-0 items-center gap-2">
               <Image
                 src="/assets/img/tta.png"
                 alt="Travel Trust Association"
-                className="h-12"
                 width={96}
                 height={61}
+                className="h-8 w-auto sm:h-10 lg:h-12"
               />
+
               <span className="hidden lg:flex flex-col text-[13px] leading-tight">
                 <strong className="text-xl">
-                  Travel Trust <br />
+                  Travel Trust
+                  <br />
                   <small>Association</small>
                 </strong>
               </span>
             </div>
 
+            {/* Phone */}
             <a
               href="tel:02039947646"
-              className="flex items-center gap-2 rounded-2xl bg-blue-900 px-5 py-2.5 font-bold text-white"
+              aria-label="Call Plenty Holidays"
+              className="
+          flex
+          shrink-0
+          items-center
+          justify-center
+          rounded-xl
+          bg-blue-900
+          p-2.5
+          text-white
+          sm:px-4
+          sm:py-2.5
+        "
             >
               <Phone size={18} />
-              <span className="hidden flex-col text-left lg:flex">
+
+              {/* Desktop phone information */}
+              <span className="hidden lg:flex flex-col text-left ml-2">
                 <span className="text-2xl font-bold leading-none">
                   0203 994 7646
                 </span>
+
                 <small className="text-[13px] font-medium">
-                  Everyday <span className="font-bold text-lg">8 am</span> to{" "}
-                  <span className="font-bold text-lg">11 pm</span>
+                  Everyday <span className="text-lg font-bold">8 am</span> to{" "}
+                  <span className="text-lg font-bold">11 pm</span>
                 </small>
               </span>
             </a>
 
+            {/* Mobile menu */}
             <button
+              type="button"
               onClick={() => setMenuOpen((o) => !o)}
-              aria-label="Toggle menu"
-              className="text-3xl lg:hidden"
+              aria-label={menuOpen ? "Close menu" : "Open menu"}
+              aria-expanded={menuOpen}
+              className="
+          flex
+          shrink-0
+          items-center
+          justify-center
+          p-1
+          text-gray-800
+          lg:hidden
+        "
             >
-              {menuOpen ? <X size={30} /> : <Menu size={30} />}
+              {menuOpen ? <X size={28} /> : <Menu size={28} />}
             </button>
           </div>
         </div>
 
-        {/* Main nav */}
+        {/* Main Navigation */}
         <nav
-          className={`${
-            menuOpen ? "block" : "hidden"
-          } border-t border-gray-100 lg:block lg:border-t-0`}
+          className={`
+      ${menuOpen ? "block" : "hidden"}
+      border-t
+      border-gray-100
+      lg:block
+      lg:border-t-0
+    `}
         >
-          {/* <ul className="flex flex-col items-center gap-4 py-4 font-bold lg:flex-row lg:flex-wrap lg:justify-center lg:gap-10">
-            {NAV_LINKS.map((link) => (
-              <li key={link}>
-                <a href="#" className="group flex items-center gap-1 ">
-                  <span className="relative animated-underline">
-                    {link}
-                    <span className="absolute -bottom-1 left-0 h-\[2px\] w-0 bg-current transition-all duration-300 group-hover:w-full" />
-                  </span>
-                  <ChevronDown size={14} />
-                </a>
-              </li>
-            ))}
-          </ul>
-          <ul className="flex flex-col items-center gap-4 py-4 font-bold lg:flex-row lg:flex-wrap lg:justify-center lg:gap-10">
-            {NAV_LINKS.map((link) => (
-              <li key={link.label}>
-                <a href="#" className="group flex items-center gap-1">
-                  <span className="relative animated-underline">
-                    {link.label}
-                    <span className="absolute -bottom-1 left-0 h-0.5 w-0 bg-current transition-all duration-300 group-hover:w-full" />
-                  </span>
-                  {link.hasDropdown && <ChevronDown size={14} />}
-                </a>
-              </li>
-            ))}
-          </ul> */}
           <Navbar />
         </nav>
       </div>
