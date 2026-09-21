e26200873f9744435ad7052cbb568fa567eb9cdc
saahil khan <saahilslaptop@gmail.com>
2026-09-21 18:23:57 +0530

    Fix Fronttest folder casing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { permanentRedirect, redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default function home() {
   permanentRedirect("/front")}
